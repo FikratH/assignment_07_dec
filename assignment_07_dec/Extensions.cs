314e4edf1d682a5e811901ff5742af7647620b1e
fikret2006 <Fikret.huseynov2006@gmail.com>
2022-12-07 16:37:38 +0400

    Добавьте файлы проекта.

---
+++ assignment_07_dec/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_07_dec
+{
+    internal static class Extensions
+    {
+        public static string CustomContains(this string str,string checkedStr)
+        { 
+            string strLow = str.ToLower(); //hello world
+            string checkedStrLow = checkedStr.ToLower(); //rld
+            int checkIndex = 0;
+            int i;
+            for (i = 0; i < strLow.Length; i++)
+            {
+                if (strLow[i] == checkedStrLow[checkIndex])
+                {
+                    if (checkIndex == checkedStrLow.Length - 1)
+                    {
+                        return $"The sentence contains '{checkedStr}'.";
+                    }
+                    checkIndex++;
+                }
+                else
+                {
+                    checkIndex = 0;
+                    i--;
+                }
+            }
+            return $"The sentence does NOT contain '{checkedStr}'."; ;
+        }
+    }
+}
---
+++ assignment_07_dec/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace assignment_07_dec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a sentence:");
+            string? str = Console.ReadLine();
+            Console.WriteLine("Please, enter a string to check if it a sentence contains it: ");
+            string? str2 = Console.ReadLine();
+            Console.WriteLine(str?.CustomContains(str2)); 
+        }
+    }
+}
